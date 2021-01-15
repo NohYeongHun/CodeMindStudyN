bd31e6f8263ef542f86fa29a1e244de3e692dab5
noyounghun <dkzndk1100@naver.com>
2021-01-15 15:03:01 +0900

    so hard C..++

--- Deadline.cpp
+++ Deadline.cpp
@@ -16,7 +16,12 @@ int n=5;
 struct DStruct{ //구조체 선언
     int st;
     float ct;
+    int getintStruct(){ return st; }
+    int getfloatStruct(){ return ct; }
+    void setintStruct(int stex){ st=stex; }
+    void setfloatStruct(int ctex){ ct=ctex; }
 };
+
 
 class A{ // 클래스 체크
     private:
@@ -28,13 +33,17 @@ class A{ // 클래스 체크
             this->b = b; 
             //this->a 멤버 a는 매개 변수
         }
+        int getA(){return a;}
+        void setA(int aa){ a=aa;}
+        int getB(){return b;}
+        void setB(int bb){ b=bb;}
         void printing(){
             cout <<this ->a <<endl;
             cout <<this ->b <<endl;  
         }   
         void ClassFunc(A at){ //클래스 심볼
-            for(int i = 0; i<at.a; i++){
-            for(int j = 0; j<at.b; j++){
+            for(int i = 0; i<at.getA(); i++){
+            for(int j = 0; j<at.getB(); j++){
             cout<<"* ";
         }
             cout<<"\n";
@@ -53,6 +62,10 @@ S Mincheck(const int& lv, const int& rv){ //템플릿 사용
 union UBox{ // Union 선언
     bool a;
     int x,y;
+    int getUnionX(){ return x; }
+    void setUnionX(int ex){ x = ex; }
+    int getUnionY(){ return y; }
+    void setUnionY(int ey){ y = ey; }
 };
 
 int FTest(int a, int b){ //int형 파라미터 심볼
@@ -74,24 +87,23 @@ float FReturn(float a, float b){ //float 파라미터 심볼 생성
 }
 
 void setDStruct(struct DStruct p, int a, float b){ //구조체 파라미터
-    p.st =a;
-    p.ct =b;
-    if(p.st == p.ct){
+    if(p.getintStruct() == p.getfloatStruct()){
         cout<< "p.st == p.ct"<<endl;
-        cout << p.st <<endl;
+        cout << p.getintStruct() <<endl;
+        p.setintStruct(p.getfloatStruct()+1);
     }else{
         cout<< "not equal" << endl;
-        cout <<"pst: " <<p.st <<endl;
-        cout <<"pct: " <<p.ct <<endl;
+        cout <<"pst: " <<p.getintStruct() <<endl;
+        cout <<"pct: " <<p.getfloatStruct() <<endl;
     } 
     
 }
 
 void UnionTest(UBox up,int a, int b){ // 공용체 테스트
-    if(up.x < b){
-        up.x = b;
+    if(up.getUnionX() < b){
+        up.setUnionX(b);
     }else{
-        up.x = a;
+        up.setUnionY(a);
     }
     
 }
@@ -162,7 +174,7 @@ void NullCheck(int a, int b){ //null check
 int main(){
     UBox u1;
     UnionTest(u1,3,2);
-    cout << u1.x << endl;
+    cout << u1.getUnionX() << endl;
     /*-------*/
     FParam(FTest);
     float tt =FReturn(5,3);
