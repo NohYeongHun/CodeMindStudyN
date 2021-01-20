94e8de5e24562218f6e35b3d97b607e3da1ce0f5
ny <dkzndk1100@naver.com>
2021-01-20 22:58:55 +0900

    home study

---
+++ example/sugar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+
+int main(){
+    int N;
+    int count = 0;
+    scanf("%d",&N);
+
+    if(N%5==0){
+        count = N/5;
+    }else if(N%5==1 && N/5>=1){
+        count = N/5+1;
+    }else if(N%5==2 && N/5>=2){
+        count = N/5+2;
+    }else if(N%5==3){
+        count = N/5+1;
+    }else if(N%5==4 && N/5>=1){
+        count = N/5+2;
+    }else{
+        count =-1;
+    }
+
+    printf("%d", count);
+}
