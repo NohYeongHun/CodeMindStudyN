4a308b1e7617409ee970446d754b7f1365360751
ny <dkzndk1100@naver.com>
2021-01-25 23:25:12 +0900

    0125 Study

--- Algorithm/arrayEx3.cpp
+++ Algorithm/arrayEx3.cpp
@@ -86,3 +86,63 @@ int main(){
         cout<<endl;
      }
 }
+
+/* 모범소스 1
+#include <stdio.h>
+int map[12][12];
+
+void back(int r, int c)
+{
+	if(map[r][c] == 2)
+	{
+			map[r][c] = 9;
+			return ;
+	}
+	map[r][c] = 9;
+
+	if(map[r][c+1] != 1)
+		back(r, c+1);
+	else if(map[r+1][c] != 1)
+		back(r+1, c);
+}
+
+int main()
+{
+	int i, j;
+	for(i = 1; i <= 10; i++)
+		for(j = 1; j <= 10; j++)
+			scanf("%d", &map[i][j]);
+	back(2, 2);
+
+	for(i = 1; i <= 10; i++,puts(""))
+		for(j = 1; j <= 10; j++)
+			printf("%d ", map[i][j]);
+}
+*/
+
+/* 모범소스 2
+#include<stdio.h> 
+int main() 
+{ 
+    int map[11][11],x=2,y=2,i,j; 
+    for(i=1;i<=10;i++) 
+        for(j=1;j<=10;j++) 
+            scanf("%d",&map[i][j]); 
+    while(map[x][y]!=2) 
+    { 
+        map[x][y]=9; 
+        if(map[x][y+1]!=1) y+=1; 
+        else if(map[x+1][y]!=1) x+=1; 
+        else break; 
+    } 
+    map[x][y]=9; 
+    for(i=1;i<=10;i++) 
+    { 
+        for(j=1;j<=10;j++) 
+            printf("%d ",map[i][j]); 
+        printf("\n"); 
+    } 
+    return 0; 
+}
+
+*/
