e55a6ea7924e9b68a34e2410859ed9309a1fd0e2
ny <dkzndk1100@naver.com>
2021-01-27 00:49:31 +0900

    0126 Study

--- Test/test.cpp
+++ Test/test.cpp
@@ -2,9 +2,28 @@
 using namespace std;
 int main(){
     int A,B,C;
-    int X[10];// 개수 배열
-    int T[10];// 출력 배열
+    int Sum, cnt=0;
+    int X[10]={};
+    int T[10]={};
     cin>>A>>B>>C;
+         Sum = A*B*C; 
+    for(int i=0;i<=9;i++){
+        if(Sum!=0){ 
+            X[i]=Sum%10; 
+            cnt++;
+        }
+        Sum = Sum/10;
+    }
     
-    
+    for(int i=0;i<=9;i++){
+        for(int j=0; j<cnt;j++){
+            if(X[j]==i){
+                T[i]+=1;
+            }
+        }
+    }
+
+    for(int i=0;i<=9;i++){
+        cout<<T[i]<<"\n";
+    }
 }
