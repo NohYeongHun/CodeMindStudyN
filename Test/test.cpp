3b91dc7dc90f2817cc7ebd9e9dc28e67cf1529da
ny <dkzndk1100@naver.com>
2021-02-06 16:28:03 +0900

    0206 Study

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,34 +1,26 @@
 /*
-스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다. 
-스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 
-제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+문제
+크기가 N인 수열 A = A1, A2, ..., AN이 있다. 
+수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다. 
+Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다.
+ 그러한 수가 없는 경우에 오큰수는 -1이다.
 
-1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 
-이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 임의의 수열이 주어졌을 때
-스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 
-어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
+예를 들어, A = [3, 5, 2, 7]인 경우 
+NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다.
+ A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
 
 입력
-첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다.
- 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다.
- 물론 같은 정수가 두 번 나오는 일은 없다.
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 
+둘째에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
 
 출력
-입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. 
-push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
-
-
-1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 
-이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 
-
-임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 
-있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
+총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
 */
 #include <iostream>
 #include <string>
 #include <cstring>
 using namespace std;
-#define STACK_SIZE 100000
+#define STACK_SIZE 1000000
 typedef int element;
 
 element stack[STACK_SIZE]; // 1차원 배열 스택 선언
@@ -76,47 +68,21 @@ element peek(){
 		}
 	else return stack[top];
 }
-//push와 pop의 순서를 배합해서 입력한 수열을 만들 수 있는지 확인
-int main(){
-	//선언
-	int N; // 1~n까지의 범위
-	//입력
-	cin>>N;
-	int i=0,j=0,c=0;
-	char *sign = (char *)malloc(sizeof(char)*N*2);
-	int *arr = (int *)malloc(sizeof(int)*N);
-		for(i=0; i<N;i++){
-			cin>>arr[i];
-	}
-	i=1;
-	// 로직 완성 불가능한경우 NO 출력.
-	while(1){
-		if(arr[j]==stack[top]){
-			pop();
-			sign[c]='-';
-			c++;
-			j++;
-		}
-		else if(arr[j]!=stack[top]){
-			if(i>N){
-				cout<<"NO\n";
-				break;
-			}
-			push(i);
-			sign[c]='+';
-			c++;
-			i++;
-		}
-		if(isEmpty() && j==N){
-			c=0;
-			while(c!=N*2){
-				cout<<sign[c]<<"\n";
-				c++;
-			}
-			break;
-		}
 
-	}
-	free(arr);
-	free(sign);
+/*
+4
+3527 => 5 7 7 -1
+오큰수 3은 5 2 7 과 다비교를해야함.
+*/
+int main(){
+	int N; // 크기가 N인 수열
+	int i=0, j=0, c=1;
+	cin>>N; //
+	int *nge = (int*)malloc(sizeof(int)*N); //일반 수열
+	int *obig = (int*)malloc(sizeof(int)*N);//오큰수
+	for(i=0; i<N;i++){
+		cin>>nge[i];
+		cout<<"nge["<<i<<"] : "<<nge[i]<<"\n";
+	}	
+
 }
