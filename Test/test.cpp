a8298454b3cf1fff2d1ef26e0a4178adc4831399
ny <dkzndk1100@naver.com>
2021-03-01 16:16:00 +0900

    0301 진행중

--- Test/test.cpp
+++ Test/test.cpp
@@ -2,31 +2,70 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define MIN(a,b) ((( a < b )) ? (a) : (b))
 using namespace std;
+
+
+
 /* 
-2 테스트 케이스 개수
-1,1,1,2,2,3,4,5,7,9
-점화식 만들기 => 메모이제이션
-fado(n)= fado(n-1)+fado(n-5)
+1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+점화식-
+
+메모이제이션-
+
  */
-int T=0;
+/* 
+빨강 = 0
+초록 = 1
+파랑 = 2
+ */
+
 int N=0;
-long long int dp[101]={0,1,1,1,2,2};
-long long int fado(int n){
-    if(n<=5)  return dp[n];
-    if(n>=6){
+int dp[1001]={0,};
+int house[1001][3]={0,};
+int RGB(int n){
+        int temp =0;
+    if(n==1){
+        /* house[n][0]<house[n][1] */
         if(dp[n]!=0) return dp[n];
         else{
-        dp[n]= fado(n-1)+fado(n-5);
-        return fado(n-1)+fado(n-5);
+            if(house[n][0]<house[n][1]){
+                if(house[n][0]<house[n][2]){
+                    dp[n] = 0;
+                    return 0;
+                }else{
+                    dp[n] = 2;
+                    return 2;
+                }
+            }else{
+                if(house[n][1]<house[n][2]){
+                    dp[n] = 1;
+                    return 1;
+                }else{
+                    dp[n] = 2;
+                    return 2;
+                }
+            }
         }
     }
-};
-
+    else if(n>=2){
+        if(dp[n]!=0) return dp[n];
+        else{
+            
+        }
+    }     
+    
+    
+}
 int main(){
-    cin>>T;
-    for(int i=0; i<T;i++){
-        cin>>N;
-        cout<<fado(N)<<"\n";
+    cin>>N;
+    int tem=0;
+    for(int i=1; i<=N;i++){
+        cin>>house[i][0]>>house[i][1]>>house[i][2];
+        tem +=house[i][RGB(i)];
     }
+    cout<<tem;
 }
