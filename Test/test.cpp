b68e98a5d6914812763ed671bbb57f56a2e03188
ny <dkzndk1100@naver.com>
2021-03-02 13:15:21 +0900

    0301

--- Test/test.cpp
+++ Test/test.cpp
@@ -13,59 +13,6 @@ i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아�
 초록 = 1
 파랑 = 2
 
-if(n==1){
-        if(dp[n]!=0) return dp[n];
-        else{
-            if(house[n][0]<house[n][1]){
-                if(house[n][0]<house[n][2]){
-                    dp[n] = 0;
-                    return 0;
-                }else{
-                    dp[n] = 2;
-                    return 2;
-                }
-            }else{
-                if(house[n][1]<house[n][2]){
-                    dp[n] = 1;
-                    return 1;
-                }else{
-                    dp[n] = 2;
-                    return 2;
-                }
-            }
-        }
-    }
-
-else if(n>=2){
-        if(dp[n]!=0) return dp[n];
-        else{
-            if(RGB(n-1)==0){
-                if(house[n][1]<house[n][2]){
-                    dp[n] = 1;
-                    return 1;
-                }else{
-                    dp[n] = 2;
-                    return 2;
-                }
-            }else if(RGB(n-1)==1){
-                if(house[n][0]<house[n][2]){
-                    dp[n] = 0;
-                    return 0;
-                }else{
-                    dp[n] = 2;
-                    return 2;
-                }
-            }else{
-                if(house[n][0]<house[n][1]){
-                    dp[n] = 0;
-                    return 0;
-                }else{
-                    dp[n] = 1;
-                    return 1;
-                }                
-            } 
-        }
-    }
 
  */
 #include <math.h>
@@ -73,21 +20,32 @@ else if(n>=2){
 #include <vector>
 #include <algorithm>
 using namespace std;
-int N=0;
-int dp[1001]={0,};
-int house[1001][4]={0,};
-int RGB(int n){
-    int temp =0;
-    if(dp[n]!=0) return dp[n];
-    else{
-
-    }   
-}   
-
-int main(){
-    cin>>N;
-    int tem=0;
-    for(int i=1; i<=N;i++){
-        cin>>house[i][1]>>house[i][2]>>house[i][3];
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+int N, Solution;
+int dp[1001][3];
+int house[1001][3];
+void In(){
+    cin >> N;   
+    for(int i=1; i<=N; i++){
+        cin >> house[i][0]>>house[i][1]>>house[i][2];
     }
+    dp[0][0] = dp[0][1] = dp[0][2] =0;
+    house[0][0] = house[0][1] = house[0][2] = 0;
+}
+void Sol(){
+    for (int i=1;i <= N; i++){
+        dp[i][0] = MIN(dp[i-1][1],dp[i-1][2])+ house[i][0];
+        dp[i][1] = MIN(dp[i-1][0],dp[i-1][2])+ house[i][1];
+        dp[i][2] = MIN(dp[i-1][1],dp[i-1][0])+ house[i][2];
+    }
+    Solution = MIN(MIN(dp[N][0],dp[N][1]),dp[N][2]);
+    cout<<Solution<<"\n";
+}
+
+void Solve(){
+    In();
+    Sol();
+}
+int main(){
+    Solve();
 }
