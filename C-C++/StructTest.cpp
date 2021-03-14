3c18c788990ec40fdfa7f3e7dafef84544e037e1
ny <dkzndk1100@naver.com>
2021-03-14 13:13:44 +0900

    commit

--- C-C++/StructTest.cpp
+++ C-C++/StructTest.cpp
@@ -10,18 +10,6 @@ struct DStruct{
     float ct;
 };
 
-<<<<<<< HEAD
-void setDStruct(struct DStruct *p, int a, float b){
-    p->st = a;
-    p->ct = b;
-    if(p->st == p->ct){
-        cout<< "p.st == p.ct"<<endl;
-        cout << p->st <<endl;
-    }else{
-        cout<< "not equal" << endl;
-        cout <<"p->st: " <<p->st <<endl;
-        cout <<"p->ct: " <<p->ct <<endl;
-=======
 void setDStruct(struct DStruct p, int a, float b){
     p.st =a;
     p.ct =b;
@@ -32,7 +20,6 @@ void setDStruct(struct DStruct p, int a, float b){
         cout<< "not equal" << endl;
         cout <<"pst: " <<p.st <<endl;
         cout <<"pct: " <<p.ct <<endl;
->>>>>>> main
     } 
     
 }
@@ -42,13 +29,8 @@ int main(){
     // StructTest(Dt, a,b);
     // cout<< Dt.st << endl;
     struct DStruct d1;
-<<<<<<< HEAD
-    setDStruct(&d1,a,b);
-    cout << &d1 <<endl;
-=======
     setDStruct(d1,a,b);
-    cout << d1.ct <<endl;
->>>>>>> main
+    cout << &d1 <<endl;
     cout << "set st : "<< a <<endl;
     cout << "set ct : "<< b <<endl;
     return 0 ;
--- Test/test.cpp
+++ Test/test.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 using namespace std;
 
+
+
 int main(){
-   
+   cout<<"hi";
 }
