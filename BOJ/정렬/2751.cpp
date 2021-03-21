116baceeef21d04875d7bd4d8434e098130a63e0
ny <dkzndk1100@naver.com>
2021-03-21 17:55:56 +0900

    0321 Study
    공모전 끝났다.. 다시 알고리즘 공부 열심히

--- BOJ/정렬/2751.cpp
+++ BOJ/정렬/2751.cpp
@@ -1,22 +1,22 @@
-//boj.kr/2751
+// boj.kr/2751
 #include <math.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 #define MAX 1000000
-void swap(int arr[], int a, int b){
-    int tmp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = tmp;
-}
+
 
 void threeSort(int arr[], int front, int mid, int rear){
     if(arr[front]>arr[mid]) swap(arr,front,mid);
     if(arr[mid]>arr[rear]) swap(arr,mid,rear);
     if(arr[front]>arr[mid]) swap(arr, front, mid);
 }
-
+void swap(int arr[], int a, int b){
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
 void quick_sort(int arr[], int front, int rear){ //rear = 끝 인덱스, front = 처음 인덱스
     int i, j, pivot, mid = front+(rear-front)/2;
     threeSort(arr, front, mid, rear);
@@ -26,18 +26,22 @@ void quick_sort(int arr[], int front, int rear){ //rear = 끝 인덱스, front =
         i = front; // 맨앞은 이미 정렬
         j = rear-1; // 맨뒤는 이미 정렬
     while(true){
-        while(arr[++i]<pivot && i<rear);  //arr[++(front)]<pivot and i<rear
-        while(arr[--j]>pivot && front<j); //arr[--(rear-1)]<pivot and i<rear
-        if(i>=j) break;
-        swap(arr, i, j);
+        //왼쪽 pivot보다 작은곳 pivot보다 큰값이 왼쪽에 나오면 종료.
+        while(arr[++i]<pivot && i<rear);  //pivot보다 작을 동안 =>arr[++(front)]<pivot and i<rear ==제약조건
+        //오른쪽 pivot보다 큰곳 pivot보다 작은 값이 오른쪽에 나오면 종료.
+        while(arr[--j]>pivot && front<j); //pivot보다 클 동안 arr[--(rear-1)]<pivot and fornt<j ==제약조건       
+        if(i>=j) break; //while문 종료시 i값이 j보다 크거나 같으면 break;
+        swap(arr, i, j); // while문 종료시 arr[i]값과 arr[j]값을 교환한다.
     }
     swap(arr, i, rear-1);
     quick_sort(arr, front, i-1);
-    quick_sort(arr, i+1, rear);
+    quick_sort(arr, i+1, rear);  
     }
 }
 int N=0;
 int main(){
+    cin.tie(NULL); 
+    ios::sync_with_stdio(false);
     int N=0;
     cin>>N;
     vector<int> T(N,0);
@@ -47,16 +51,3 @@ int main(){
     for(int i=0; i<N;i++)
         cout<<T[i]<<"\n";
 }
-/* 
-int N=0;
-int main(){
-    int N=0;
-    cin>>N;
-    int T[N];
-    for(int i=0; i<N;i++)
-        cin>>T[i];
-    quick_sort(T,0,N-1);
-    for(int i=0; i<N;i++)
-        cout<<T[i]<<"\n";
-}
- */
