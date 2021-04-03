ea19965885d5b08f17a2ed8a8fe059c940fd076e
ny <dkzndk1100@naver.com>
2021-04-03 21:56:37 +0900

    ss

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,19 +1,24 @@
-#include <math.h>
+/* 
+삽입정렬
+최악의 경우 => 시간복잡도 O(n^2) N + (N-1) .. .. 0 
+일반적인 경우에 삽입정렬이 버블과, 선택정렬과 비교해서 가장 적게 연산한다.
+삽입정렬이 최악의 경우 => 10,9,8,7,6,5,4,3,2,1
+거의 정렬된 상태라면 가장 효율적임.
+ex) 2 3 4 5 6 7 8 9 10
+ */
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 #define MAX 10000
 
 int N;
 int array[MAX+1];
 
-
 // 정렬의 기본이되는 함수 인수 두개의 위치를 바꿔줌.
-void *swap(int arr[], int a, int b){
-    int temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+void *swap(int &arr1, int &arr2){
+    int temp =0;
+    temp = arr1;
+    arr1 = arr2;
+    arr2 = temp;
 }
 void input(){
     cin>>N;
@@ -21,51 +26,15 @@ void input(){
         cin>>array[i];
     }
 }
-
-void quick_sort(int *arr,int front, int end){
-    if (front >= end) return;
-    int i,j=0;
-    int pivot = front;
-    i = front+1;
-    j = end;
-    while(i<=j){ // 엇갈릴때 까지 반복
-        while(arr[i]<=arr[pivot]){ // pivot 기준 왼쪽 => 오른쪽arr[i]>arr[pivot]인 i 인덱스 찾기.
-            i++;
-        }
-        while(arr[j]>=arr[pivot] && j>front){ // 맨 오른쪽 => 왼쪽arr[j]<arr[pivot]인 j 인덱스 찾기.
-            j--;
-        }
-        if(i>j){ // 만약 i값이 j값보다 크다면
-            swap(arr,pivot,j); // swap(arr[pivot], arr[j])
-        }else{
-            swap(arr,i,j); // 아닌 경우엔 arr[i]>pivot && arr[j]<arr[pivot]인 상황이므로 둘의 값을 변경함.
+void solve(){
+    int j = 0;
+    for(int i=0; i < N-1; i++){ // j와 j+1의 대소비교를 해서 위치를 바꾸므로 N-1까지만 비교하면 된다.
+        j=i; // j=0부터 시작.
+        while(array[j]>array[j+1]){ // array[j]> array[j+1]이면 둘의 위치를 바꾸고 array[j] <=array[j+1]일때 까지 j-- why? => 정렬을 위해.
+            swap(array[j],array[j+1]);
+            j--; // array[j] <=array[j+1]일때 까지 j-- why? => 정렬을 위해.
         }
     }
-    quick_sort(arr,front,j-1); //현재 pivot값은 j인덱스에 있으므로 front ~ j-1 까지 정렬해줌.
-    quick_sort(arr,j+1,end); // 현재 pivot값은 j인덱스에 있으므로 j+1~end 까지 정렬해줌.
-}
-//내림차순
-void quick_sort_reverse(int *arr,int front, int end){
-    if (front >= end) return;
-    int i,j=0;
-    int pivot = front;
-    i = front+1;
-    j = end;
-    while(i<=j){ // 엇갈릴때 까지 반복
-        while(arr[i]>=arr[pivot]){ // pivot 기준 왼쪽 => 오른쪽arr[i]<arr[pivot]인 i 인덱스 찾기.
-            i++;
-        }
-        while(arr[j]<=arr[pivot] && j>front){ // 맨 오른쪽 => 왼쪽arr[j]>arr[pivot]인 j 인덱스 찾기.
-            j--;
-        }
-        if(i>j){ // 만약 i값이 j값보다 크다면
-            swap(arr,pivot,j); // swap(arr[pivot], arr[j])
-        }else{
-            swap(arr,i,j); // 아닌 경우엔 arr[i]>pivot && arr[j]<arr[pivot]인 상황이므로 둘의 값을 변경함.
-        }
-    }
-    quick_sort_reverse(arr,front,j-1); //현재 pivot값은 j인덱스에 있으므로 front ~ j-1 까지 정렬해줌.
-    quick_sort_reverse(arr,j+1,end); // 현재 pivot값은 j인덱스에 있으므로 j+1~end 까지 정렬해줌.
 }
 void print(){
     for(int i=0;i<N;i++){
@@ -75,7 +44,6 @@ void print(){
 int main()
 {   
     input();
-    //quick_sort(array,0,N-1);
-    quick_sort_reverse(array,0,N-1);
+    solve();
     print();
 }
